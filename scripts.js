2fa9c0ddf2999e07ddb0e6399bde7dc0e3def032
alpesh <alpeshc101020@gmail.com>
2024-10-17 16:07:30 +0530

    doneee

--- scripts.js
+++ scripts.js
@@ -1,94 +1,87 @@
-
-
-
-
 function showanswer(element) {
-    let answer = document.querySelectorAll('.faq-answer')
-    answer.forEach((item) => {
-        if (item !== element.nextElementSibling) {  
-            
-            item.classList.remove('faq-answer2')
-            item.classList.remove('active-answer')
-            console.log(item)
-            console.log(element)
-        }
+  let answer = document.querySelectorAll(".faq-answer");
+  answer.forEach((item) => {
+    if (item !== element.nextElementSibling) {
+      item.classList.remove("faq-answer2");
+      item.classList.remove("active-answer");
+      console.log(item);
+      console.log(element);
+    }
+  });
 
-       
-
-
-    })
-    
-    element.nextElementSibling.classList.add('faq-answer2')
-    element.nextElementSibling.classList.toggle('active-answer')
+  element.nextElementSibling.classList.add("faq-answer2");
+  element.nextElementSibling.classList.toggle("active-answer");
 }
 
-
-
-let containers = document.querySelectorAll('.feedback-container')
-let image1 = document.querySelectorAll('.outer-img-container .feedback-img-1')
-let image2 = document.querySelectorAll('.outer-img-container .feedback-img-2')
-let count = 0
+let containers = document.querySelectorAll(".feedback-container");
+let image1 = document.querySelectorAll(".outer-img-container .feedback-img-1");
+let image2 = document.querySelectorAll(".outer-img-container .feedback-img-2");
+let count = 0;
 
 function toggleReview() {
-    
-    containers.forEach((item) => {
-    item.classList.remove('active-review-container')
-    })
-    
+  containers.forEach((item) => {
+    item.classList.remove("active-review-container");
+  });
 
-    image1.forEach((item) => {
-    item.classList.remove('active-image')
-})
-    image2.forEach((item) => {
-    item.classList.remove('active-image')
-})
+  image1.forEach((item) => {
+    item.classList.remove("active-image");
+  });
+  image2.forEach((item) => {
+    item.classList.remove("active-image");
+  });
 
-
-
-
-image1[count].classList.add('active-image')
-image2[count].classList.add('active-image')
-containers[count].classList.add('active-review-container')
-
-
-
+  image1[count].classList.add("active-image");
+  image2[count].classList.add("active-image");
+  containers[count].classList.add("active-review-container");
 }
 
-
-
-
-
-
-toggleReview()
+toggleReview();
 
 function nextReview() {
-    if (count == 3) {
-        count = 0
-    }
-    else {
-         count++
-    }
+  if (count == 3) {
+    count = 0;
+  } else {
+    count++;
+  }
 
-    toggleReview()
-   
+  toggleReview();
 }
 
 function prevReview() {
-    if (count == 0) {
-        count = 3
-    }
-    else {
-         count--
-    }
+  if (count == 0) {
+    count = 3;
+  } else {
+    count--;
+  }
 
-    toggleReview()
-   
+  toggleReview();
 }
 
+let feedback = document.querySelector(".feedback");
+let feedbackcontainer = document.querySelector(".feedback-container");
 
-let feedback = document.querySelector('.feedback')
-let feedbackcontainer = document.querySelector('.feedback-container')
+feedback.style.height = `${window.getComputedStyle(feedbackcontainer).height}`;
 
-feedback.style.height = `${window.getComputedStyle(feedbackcontainer).height}`
 
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
